a337dedd16b5359f83959b4922651c4545b376ed
purple-mountain <lmwrong92@gmail.com>
2024-07-11 14:18:55 +0000

    simple 200 response

--- app/server.go
+++ app/server.go
@@ -6,6 +6,33 @@ import (
 	"os"
 )
 
+const HTTP_VERSION = 1.1
+
+func getResponse(req []byte) string {
+	res := fmt.Sprintf("HTTP/%1.1f 200 OK\r\n\r\n", HTTP_VERSION)
+
+	return res
+}
+
+func handleClient(conn net.Conn) error {
+	buffer := make([]byte, 1024)
+
+	_, err := conn.Read(buffer)
+	if err != nil {
+		return err
+	}
+
+	res := getResponse(buffer)
+
+	conn.Write([]byte(res))
+
+	// GET / HTTP / 1.1
+
+	// HTTP/1.1 200 OK\r\n\r\n
+
+	return nil
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -13,9 +40,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+
+	handleClient(conn)
+
+	// conn.Read()
 }
